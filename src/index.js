af470a450ecd1ff09fdf7ef96cdaadb89fd4f2e2
Sergei Kudlai <s.kudlai@mail.ru>
2024-09-10 21:58:06 +0300

    feat:completed task snake-matrix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+module.exports = function towelSort(matrix = []) {
+    const result = [];
 
-// You should implement your task here.
+    for (let i = 0; i < matrix.length; i++) {
+        for (let y = 0; y < matrix[i].length; y++) {
+            const reverseIndex = i % 2 === 0 ? y : matrix[i].length - y - 1;
+            result.push(matrix[i][reverseIndex]);
+        }
+    }
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return result;
 }
+
+
